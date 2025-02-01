fd22078c08a05ce75087de5edd16a80871c0c1c2
Marek Smolana <msmolana@gmail.com>
2025-02-01 09:39:26 +0100

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error["line_number"], "column": error["column_number"], 
-            "message": error["text"], "name": error["code"], "source": "flake8" in error}
+    return {"line": error["line_number"], "column": error["column_number"],
+            "message": error["text"], "name": error["code"],
+            "source": "flake8" in error}
     
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [ format_linter_error(error) for error in errors], 
+    return {"errors": [format_linter_error(error) for error in errors],
             "path": file_path, "status": len(errors)}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file, linter_report[file]) for file in linter_report]
+    return [format_single_linter_file(file, linter_report[file]) 
+            for file in linter_report]
